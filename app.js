7e17774e5287b5d9f616576281b16166d6d541e9
Aditya <ammuaditya303@gmail.com>
2025-05-12 16:01:13 +0530

    testing image

--- app.js
+++ app.js
@@ -194,11 +194,12 @@ app.put('/categories/:categoryId', authenticationToken, upload.single('categoryI
       } catch (error) {
           return response.status(500).json({ error: error.message });
       }
+
+      await db.collection('categories').updateOne({_id: id}, {$set: {category_name: `${categoryName}`, category_image: `${categoryImage}`, item_count: itemCount}})
+      response.status(200)
+      response.json(`Category updated successfully,  imageUrl: ${categoryImage}`)
     }
 
-    await db.collection('categories').updateOne({_id: id}, {$set: {category_name: `${categoryName}`, category_image: `${categoryImage}`, item_count: itemCount}})
-    response.status(200)
-    response.json(`Category updated successfully,  imageUrl: ${categoryImage}`)
   } else {
     response.status(400)
     response.json('Category not found')
