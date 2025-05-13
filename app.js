11d19df5d2f505020081b56e6df5d77f59045960
Aditya <ammuaditya303@gmail.com>
2025-05-13 11:38:04 +0530

    testing image file

--- app.js
+++ app.js
@@ -198,7 +198,7 @@ app.put('/categories/:categoryId', authenticationToken, upload.single('categoryI
 
     await db.collection('categories').updateOne({_id: id}, {$set: {category_name: `${categoryName}`, category_image: `${categoryImage}`, item_count: itemCount}})
     response.status(200)
-    response.json(`Category updated successfully,  imageUrl: ${request.file.path}`)
+    response.json(`Category updated successfully,  imageUrl: ${request.file}`)
 
   } else {
     response.status(400)
